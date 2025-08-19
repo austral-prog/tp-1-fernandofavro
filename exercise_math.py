a3049544843810d58f44638a88eb8312c5ffd8a9
fernandofavro <fjfavrovelo@mail.austral.edu.ar>
2025-08-19 02:17:32 -0300

    Add files via upload

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,24 @@
 def math():
     a = 57
     b = 7
+
+    # 1. Suma
+    print(a + b)
+
+    # 2. Diferencia
+    print(a - b)
+
+    # 3. Producto
+    print(a * b)
+
+    # 4. Promedio
+    print((a + b) / 2)
+
+    # 5. Cociente entero
+    print(a // b)
+
+    # 6. Resto de la división
+    print(a % b)
+
+    # 7. Valor real de la división
+    print(a / b)
